3593fb9231465086f1e1572b34c7eb8dcea44016
ryanNS3 <ryansantosn333@gmail.com>
2024-11-16 20:11:11 -0300

    feat:Adicionando navegação

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'fotoBanner' : "url(./assets/fotoBanner.png)",
+      }
+    },
   },
   plugins: [],
 }
